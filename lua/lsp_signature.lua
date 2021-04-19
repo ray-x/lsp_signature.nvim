986d3fa9ca1ae02666841e0b256b1f0472f72965
ray-x <rayx.cn@gmail.com>
2021-04-19 23:12:21 +1000

    multiple clients

--- lua/lsp_signature.lua
+++ lua/lsp_signature.lua
@@ -1,27 +1,27 @@
 -- Code leverage from https://github.com/nvim-lua/completion-nvim
 
-
 local vim = vim
 local api = vim.api
 local M = {}
 
 local manager = {
-  insertChar          = false,  -- flag for InsertCharPre event, turn off imediately when performing completion
-  insertLeave         = false,  -- flag for InsertLeave, prevent every completion if true
-  changedTick         = 0,      -- handle changeTick
-  confirmedCompletion = false,  -- flag for manual confirmation of completion
+  insertChar = false, -- flag for InsertCharPre event, turn off imediately when performing completion
+  insertLeave = false, -- flag for InsertLeave, prevent every completion if true
+  changedTick = 0, -- handle changeTick
+  confirmedCompletion = false -- flag for manual confirmation of completion
 }
 function manager.init()
-  manager.insertLeave         = false
-  manager.insertChar          = false
+  manager.insertLeave = false
+  manager.insertChar = false
   manager.confirmedCompletion = false
 end
 
-
 local check_trigger_char = function(line_to_cursor, trigger_character)
-  if trigger_character == nil then return false end
+  if trigger_character == nil then
+    return false
+  end
   for _, ch in ipairs(trigger_character) do
-    local current_char = string.sub(line_to_cursor, #line_to_cursor-#ch+1, #line_to_cursor)
+    local current_char = string.sub(line_to_cursor, #line_to_cursor - #ch + 1, #line_to_cursor)
     if current_char == ch then
       return true
     end
@@ -36,50 +36,77 @@ local signature = function()
   local pos = api.nvim_win_get_cursor(0)
   local line = api.nvim_get_current_line()
   local line_to_cursor = line:sub(1, pos[2])
-  if vim.lsp.buf_get_clients() == nil then return end
+  if vim.lsp.buf_get_clients() == nil then
+    return
+  end
 
   local triggered
+  local signature_cap = false
+  local hover_cap = false
+
+  local triggered_chars = {}
   for _, value in pairs(vim.lsp.buf_get_clients(0)) do
-    if value.resolved_capabilities.signature_help == false or
-      value.server_capabilities.signatureHelpProvider == nil then
-      return
+    if value.resolved_capabilities.signature_help == true or value.server_capabilities.signatureHelpProvider ~= nil then
+      signature_cap = true
     end
 
-    if value.resolved_capabilities.hover == false then return end
-      triggered = check_trigger_char(line_to_cursor,
-        value.server_capabilities.signatureHelpProvider.triggerCharacters)
+    if value.resolved_capabilities.hover == true then
+      hover_cap = true
+    end
+
+    if
+      value.server_capabilities.signatureHelpProvider ~= nil and
+        value.server_capabilities.signatureHelpProvider.triggerCharacters ~= nil
+     then
+      triggered_chars = value.server_capabilities.signatureHelpProvider.triggerCharacters
+    elseif value.server_capabilities.signature_help_trigger_characters ~= nil then
+      triggered_chars = value.server_capabilities.signature_help_trigger_characters
+    end
+    triggered = check_trigger_char(line_to_cursor, triggered_chars)
+  end
+
+  if signature_cap == false or hover_cap == false then
+    return
   end
 
   if triggered then
     -- overwrite signature help here to disable "no signature help" message
     local params = vim.lsp.util.make_position_params()
-    vim.lsp.buf_request(0, 'textDocument/signatureHelp', params, function(err, method, result, client_id)
-      local client = vim.lsp.get_client_by_id(client_id)
-      local handler = client and client.handlers['textDocument/signatureHelp']
-      if handler then
+    vim.lsp.buf_request(
+      0,
+      "textDocument/signatureHelp",
+      params,
+      function(err, method, result, client_id)
+        local client = vim.lsp.get_client_by_id(client_id)
+        local handler = client and client.handlers["textDocument/signatureHelp"]
+        if handler then
           handler(err, method, result, client_id)
           return
+        end
+        if not (result and result.signatures and result.signatures[1]) then
+          return
+        end
+        local lines = vim.lsp.util.convert_signature_help_to_markdown_lines(result)
+        if vim.tbl_isempty(lines) then
+          return
+        end
+        local bufnr, _ =
+          vim.lsp.util.focusable_preview(
+          method .. "lsp_signature",
+          function()
+            -- TODO show popup when signatures is empty?
+            lines = vim.lsp.util.trim_empty_lines(lines)
+            return lines, vim.lsp.util.try_trim_markdown_code_blocks(lines)
+          end
+        )
+        -- setup a variable for floating window, fix #223
+        vim.api.nvim_buf_set_var(bufnr, "lsp_floating", true)
       end
-      if not (result and result.signatures and result.signatures[1]) then
-        return
-      end
-      local lines = vim.lsp.util.convert_signature_help_to_markdown_lines(result)
-      if vim.tbl_isempty(lines) then
-        return
-      end
-      local bufnr, _ = vim.lsp.util.focusable_preview(method .. 'lsp_signature', function()
-        -- TODO show popup when signatures is empty?
-        lines = vim.lsp.util.trim_empty_lines(lines)
-        return lines, vim.lsp.util.try_trim_markdown_code_blocks(lines)
-      end)
-      -- setup a variable for floating window, fix #223
-      vim.api.nvim_buf_set_var(bufnr, "lsp_floating", true)
-    end)
+    )
   end
 end
 
-M.signature=signature
-
+M.signature = signature
 
 function M.on_InsertCharPre()
   manager.insertChar = true
@@ -94,18 +121,24 @@ function M.on_InsertEnter()
   -- setup variable
   manager.init()
 
-  timer:start(100, 200, vim.schedule_wrap(function()
-    local l_changedTick = api.nvim_buf_get_changedtick(0)
-    -- closing timer if leaving insert mode
-    if l_changedTick ~= manager.changedTick then
-      manager.changedTick = l_changedTick
-  	  signature()
-	end
-    if manager.insertLeave == true and timer:is_closing() == false then
-      timer:stop()
-      timer:close()
-    end
-  end))
+  timer:start(
+    100,
+    200,
+    vim.schedule_wrap(
+      function()
+        local l_changedTick = api.nvim_buf_get_changedtick(0)
+        -- closing timer if leaving insert mode
+        if l_changedTick ~= manager.changedTick then
+          manager.changedTick = l_changedTick
+          signature()
+        end
+        if manager.insertLeave == true and timer:is_closing() == false then
+          timer:stop()
+          timer:close()
+        end
+      end
+    )
+  )
 end
 
 -- handle completion confirmation and dismiss hover popup
@@ -115,14 +148,13 @@ function M.on_CompleteDone()
   -- signature()
 end
 
-
 M.on_attach = function()
   api.nvim_command("augroup Signature")
-    api.nvim_command("autocmd! * <buffer>")
-    api.nvim_command("autocmd InsertEnter <buffer> lua require'lsp_signature'.on_InsertEnter()")
-    api.nvim_command("autocmd InsertLeave <buffer> lua require'lsp_signature'.on_InsertLeave()")
-    api.nvim_command("autocmd InsertCharPre <buffer> lua require'lsp_signature'.on_InsertCharPre()")
-    -- api.nvim_command("autocmd CompleteDone * lua require'lsp_signature'.on_CompleteDone()")
+  api.nvim_command("autocmd! * <buffer>")
+  api.nvim_command("autocmd InsertEnter <buffer> lua require'lsp_signature'.on_InsertEnter()")
+  api.nvim_command("autocmd InsertLeave <buffer> lua require'lsp_signature'.on_InsertLeave()")
+  api.nvim_command("autocmd InsertCharPre <buffer> lua require'lsp_signature'.on_InsertCharPre()")
+  -- api.nvim_command("autocmd CompleteDone * lua require'lsp_signature'.on_CompleteDone()")
   api.nvim_command("augroup end")
 end
 
