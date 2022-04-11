48e5fb895ed541d6d8ccc3b8ac3e9f4b8ff39466
ray-x <rayx.cn@gmail.com>
2022-04-11 21:36:09 +1000

    add minium vimrc for packer

---
+++ tests/init_pack.lua
@@ -0,0 +1,40 @@
+-- init.lua
+vim.cmd([[set runtimepath=$VIMRUNTIME]])
+vim.cmd([[set packpath=/tmp/nvim/site]])
+
+local package_root = "/tmp/nvim/site/pack"
+local install_path = package_root .. "/packer/start/packer.nvim"
+
+local function load_plugins()
+  require("packer").startup({
+    function(use)
+      use({ "wbthomason/packer.nvim" })
+      use({ "neovim/nvim-lspconfig" })
+      use({ "neovim/nvim-lspconfig" })
+      use({ "ray-x/lsp_signature.nvim" })
+    end,
+    config = {
+      package_root = package_root,
+      compile_path = install_path .. "/plugin/packer_compiled.lua",
+    },
+  })
+end
+
+if vim.fn.isdirectory(install_path) == 0 then
+  vim.fn.system({
+    "git",
+    "clone",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  load_plugins()
+  require("packer").sync()
+else
+  load_plugins()
+end
+
+vim.cmd("colorscheme murphy")
+
+vim.cmd("syntax on")
+require("lspconfig").tsserver.setup({})
+require("lsp_signature").setup({})
